0b1d8ddd25b949bd90f478a8a5265329e238e72f
DenysGoncharov <fidelis18@ukr.net>
2021-05-22 11:02:10 +0300

    Commit

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonColorChoose = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(10, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(318, 139);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonColorChoose
+            // 
+            this.buttonColorChoose.Location = new System.Drawing.Point(22, 211);
+            this.buttonColorChoose.Name = "buttonColorChoose";
+            this.buttonColorChoose.Size = new System.Drawing.Size(75, 23);
+            this.buttonColorChoose.TabIndex = 1;
+            this.buttonColorChoose.Text = "ChooseColor";
+            this.buttonColorChoose.UseVisualStyleBackColor = true;
+            this.buttonColorChoose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 263);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonColorChoose);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonColorChoose;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private Button button1;
+        private ColorDialog chooseColorDialog = new ColorDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(OnClickChooseColor);
+            try
+            {
+                if (ReadSettings() == false)
+                {
+                    listBox1.Items.Add("Configrtion file is empty");
+                }
+                else 
+                {
+                    listBox1.Items.Add("Configrtion file loaded  sucsesfull");
+                    this.StartPosition = FormStartPosition.CenterScreen;
+                }
+            }
+            catch (Exception e)
+            {
+                listBox1.Items.Add("Something was wrong");
+                listBox1.Items.Add(e.Message);
+            }
+
+        }
+        void OnClickChooseColor(object Sender, EventArgs e)
+        {
+            if (chooseColorDialog.ShowDialog() == DialogResult.OK)
+                listBox1.BackColor = chooseColorDialog.Color;
+        }
+
+        void SaveSettings()
+        {
+            XmlDocument doc = loadConfirmDocment();
+
+            XmlNode node = doc.SelectSingleNode("//appSettings");
+            string[] key = new string[] {"BackGroundColor",
+                                          "ForeGrooundColor",
+                                          "Font",
+                                          "Font Stile",
+                                          "Font Size"};
+            string backColor = ColorTranslator.ToHtml(listBox1.BackColor);
+
+
+            string[] value = new string[] {backColor,
+                                          listBox1.ForeColor.Name.ToString(),
+                                          listBox1.Font.Name,
+                                          listBox1.Font.Style.ToString(),
+                                          listBox1.Font.Size.ToString()};
+            for (int i = 0; i < key.Length; i++)
+            {
+                // Обращаемся к конкретной строке по ключу.
+                XmlElement element = node.SelectSingleNode(string.Format($"/add[@key='{key[i]}']")) as XmlElement;
+
+                // Если строка с таким ключем существует - записываем значение.
+                if (element != null) { element.SetAttribute("value", value[i]); }
+                else
+                {
+                    // Иначе: создаем строку и формируем в ней пару [ключ]-[значение].
+                    element = doc.CreateElement("add");
+                    element.SetAttribute("key", key[i]);
+                    element.SetAttribute("value", value[i]);
+                    node.AppendChild(element);
+                }
+                doc.Save(Assembly.GetExecutingAssembly().Location + ".config");
+            }
+
+        }
+        bool ReadSettings()//TODO закончит считывание данных из файла конфигурации
+        {
+            NameValueCollection allAppSettings = ConfigurationManager.AppSettings;
+            if (allAppSettings.Count < 1) { return false; }
+
+                 
+            listBox1.BackColor = ColorTranslator.FromHtml(allAppSettings["BackGroundColor"]);
+            
+            listBox1.BackColor = ColorTranslator.FromHtml(allAppSettings["ForeGrooundColor"]);
+          // listBox1.Font.Name = allAppSettings["Font"];
+          // listBox1.Font.Style = allAppSettings["Font Stile"];
+          // listBox1.Font.Size= allAppSettings["Font Size"]; ;
+
+            return (true);
+        }
+        private static XmlDocument loadConfirmDocment()
+        {
+            XmlDocument doc = null;
+            try
+            {
+                doc = new XmlDocument();
+                doc.Load(Assembly.GetExecutingAssembly().Location+".config");
+                return doc;
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new Exception("No config file found", e);
+            }
+        }
+
+       
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //
+        //    ColorDialog color = new ColorDialog();
+        //    if (color.ShowDialog() == DialogResult.OK)
+        //    {
+        //        listBox1.BackColor = color.Color;
+        //        listBox1.Items.Insert(0, "BackGround Color: " + listBox1.BackColor.Name.ToString());
+        //        listBox1.Items.Insert(1, "Foreground Color: " + listBox1.ForeColor.Name.ToString());
+        //        listBox1.Items.Insert(2, "Font: " +             listBox1.Font.Name);
+        //        listBox1.Items.Insert(3, "Font Stile: "+        listBox1.Font.Style.ToString());
+        //        listBox1.Items.Insert(4, "Font Size: " +        listBox1.Font.Size.ToString());
+        //    }
+        //}
+    }
+}
